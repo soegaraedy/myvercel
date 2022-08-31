5115cf56d0aef3875e0bf35c7ef4e0c43ef1a799
sugara ady <soegaraedy@gmail.com>
2022-08-31 14:41:25 +0700

    Secondo

--- api/users.js
+++ api/users.js
@@ -1,22 +1,4 @@
-//const express = require('express');
-import express from "express";
-const router = express.Router();
-
-/**
- * GET Users
- */
-
-router.get("/", async (req, res) => {
-    try {
-        res.json({
-            status: 200,
-            message: "Get Data Successfully"
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Server Error");
-    }
-
-});
-
-export default router;
+export default function users(req, res) {
+    res.statusCode = 200;
+    res.json({ message: 'It works' });
+  }
