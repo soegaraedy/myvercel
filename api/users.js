6ca13539317bad7e6d25f1bc7f91749513d4d340
soegaraedy@gmail.com <soegaraedy@gmail.com>
2022-09-01 01:27:13 +0700

    commit one

--- api/users.js
+++ api/users.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async(req, res, next)=>{
+//End point "/" nya adalah /api/users
+router.get("/", async(req, res, next)=>{ 
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly",
   });
+});
+
+router.get("/:id", async(req, res, next)=>{
+  try {
+    
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
